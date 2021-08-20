521698301c584dd4ca36b0b0eab53af2d638fe22
Maxim B <maxim.belyaevskiy@shtormtech.ru>
2021-08-20 10:31:10 +0300

    Замечания по PR

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 def run_task2():
     user_sec = int(input("Введите количество секунд: "))
-    H = user_sec // 3600
-    M = (user_sec - H * 3600) // 60
-    S = user_sec - M * 60 - H * 3600
-    print(f'{H:02d}:{M:02d}:{S:02d}')
+    h = user_sec // 3600
+    m = (user_sec - h * 3600) // 60
+    s = user_sec - m * 60 - h * 3600
+    print(f'{h:02d}:{m:02d}:{s:02d}')
 
